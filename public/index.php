069a7570c0e86de5bedfdd4e2bbbbcd2c3655941
Levi Gonzales <lvstross@gmail.com>
2017-11-16 09:27:50 -0800

    Pattern matching urls to locate controller names and action names in the url. Further refinement will be made to allow for more flexablility.

--- public/index.php
+++ public/index.php
@@ -7,17 +7,15 @@ require '../Core/Router.php';
 $router = new Router();
 
 // Add the routes
-$router->add('/', ['controller' => 'Home', 'action' => 'index']);
-$router->add('/posts', ['controller' => 'Posts', 'action' => 'index']);
-$router->add('/posts/new', ['controller' => 'Posts', 'action' => 'new']);
+// $router->add('/', ['controller' => 'Home', 'action' => 'index']);
+// $router->add('/posts', ['controller' => 'Posts', 'action' => 'index']);
+// $router->add('/posts/new', ['controller' => 'Posts', 'action' => 'new']);
 
 // Match the requested route
 $url = $_SERVER['REQUEST_URI'];
 
 if($router->match($url)) {
-    echo "<pre>";
     var_dump($router->getParams());
-    echo "</pre>";
 }else{
     echo "No route found for URL '$url' ";
 }
