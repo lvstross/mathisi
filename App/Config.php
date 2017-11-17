fae213c5e30e7aeb57adcea6633b39ae17fa2d97
Levi Gonzales <lvstross@gmail.com>
2017-11-17 12:16:07 -0800

    Added an environment config class file

---
+++ App/Config.php
@@ -0,0 +1,35 @@
+<?php
+namespace App;
+
+class Config
+{
+    /**
+    * PDO Supported Driver
+    * @var string
+    */
+    const PDO_DRIVER = 'mysql';
+
+    /**
+    * Database host
+    * @var string
+    */
+    const DB_HOST = 'localhost';
+
+    /**
+    * Database name
+    * @var string
+    */
+    const DB_NAME = 'mvc';
+
+    /**
+    * Database user
+    * @var string
+    */
+    const DB_USER = 'root';
+
+    /**
+    * Database password
+    * @var string
+    */
+    const DB_PASSWORD = '';
+}
