2f604e77676ceb3167ae81550f1f3dacec1f7031
Levi Gonzales <lvstross@gmail.com>
2017-11-22 10:30:16 -0800

    Added an environmental error constant

--- App/Config.php
+++ App/Config.php
@@ -32,4 +32,10 @@ class Config
     * @var string
     */
     const DB_PASSWORD = '';
+
+    /**
+    * Show or hide error messages on screen
+    * @var boolean
+    */
+    const SHOW_ERRORS = true;
 }
