b4b5d0649305a648d8deb16bd38306b1a749fb7f
Levi Gonzales <lvstross@gmail.com>
2017-11-28 12:41:48 -0800

    Added a Token generating class for storing, destroying and getting hashed tokens

---
+++ Core/Token.php
@@ -0,0 +1,49 @@
+<?php 
+namespace Core;
+
+/**
+* Generate unique random tokens
+*/
+class Token
+{
+    /**
+    * The token value
+    * @var array
+    */
+    protected $token;
+
+    /**
+    * Class contructor. Create a new random token
+    *
+    * @return void
+    */
+    public function __construct($token_value = null)
+    {
+        if($token_value){
+            $this->token = $token_value;
+        }else{
+            $this->token = bin2hex(random_bytes(16));
+        }
+    }
+
+    /**
+    * Get the token value
+    *
+    * 
+    * @return string The value
+    */
+    public function getValue()
+    {
+        return $this->token;
+    }
+
+    /**
+    * Get the hashed toekn value
+    *
+    * @return string The hashed value
+    */
+    public function getHash()
+    {
+        return hash_hmac('sha256', $this->token, \App\Config::SECRET);
+    }
+}
