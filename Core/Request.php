2d724f7b55eb8c32217eb270323ecc1b13ac408f
Levi Gonzales <lvstross@gmail.com>
2018-01-29 09:35:21 -0800

    Adding request and response objects to core. Files only contain class syntax and notes for now.

---
+++ Core/Request.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Core;
+
+class Request
+{
+    // List of outputs //
+    
+    // json conversion
+    // query string
+    // server array
+    // files bag
+    // cookies bag
+    // headers - with request headers ? or response headers
+    // pathInfo '/dashboard'
+    // requesturi '/dashboard'
+    // baseUrl
+    // Method 'GET, POST'
+    // session
+    // local language
+    // default local language
+}
